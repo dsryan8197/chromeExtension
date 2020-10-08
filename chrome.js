0df631ba7cc757c26788e56bfbae7abedda24d78
Seamus Ryan <d.seamus.ryan@outlook.com>
2020-10-08 02:31:10 -0400

    j

--- chrome.js
+++ chrome.js
@@ -7,12 +7,24 @@
 //     var button = document.createElement("button");
 //     // button.innerHTML = "rename"
 //     el.appendChild(button);
-//     // document.createElement('button').appendChild( document.title );
+//     // document.createElement('button').appendChild( document'''''''.title );
 
-var button = document.getElementsByTagName("button");
+var button = document.querySelector('.clicker');
 button.addEventListener('click', changeTitle)
 
 function changeTitle() {
+    let text = document.querySelector('input').value
+    let header = document.getElementsByTagName('title')
+    // console.log(header)
+    // console.log(header[0])
+    // alert(header[0])
+    // alert(header)
+    // header[0].innerHTML = `${text}`;
+    header[0].innerText = text
 
+    // document.getElementsByTagName('title') =  header[0].innerHTML = `${text}`
+
+//    if (text) header = text;
 }
-
+let header = document.getElementsByTagName('title')
+console.log(header)
