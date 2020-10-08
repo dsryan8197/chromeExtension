20225a7055ca6d942ded35f0a496a720fb77480b
Seamus Ryan <d.seamus.ryan@outlook.com>
2020-10-08 04:09:59 -0400

    done

--- chrome.js
+++ chrome.js
@@ -1,30 +1,28 @@
-// let title = document.getElementsByTagName('title')
-// let value
-// title.innerText = value
-
-    
-//     var el = document.getElementsByTagName('html');
-//     var button = document.createElement("button");
-//     // button.innerHTML = "rename"
-//     el.appendChild(button);
-//     // document.createElement('button').appendChild( document'''''''.title );
 
 var button = document.querySelector('.clicker');
 button.addEventListener('click', changeTitle)
 
 function changeTitle() {
+    // chrome.tabs.executeScript {
     let text = document.querySelector('input').value
     let header = document.getElementsByTagName('title')
+    console.log(text)
+    console.log(header)
     // console.log(header)
     // console.log(header[0])
     // alert(header[0])
     // alert(header)
     // header[0].innerHTML = `${text}`;
     header[0].innerText = text
-
+    
     // document.getElementsByTagName('title') =  header[0].innerHTML = `${text}`
 
 //    if (text) header = text;
 }
 let header = document.getElementsByTagName('title')
 console.log(header)
+
+
+// let text = document.querySelector('input').value
+// let header = document.getElementsByTagName('title')
+
