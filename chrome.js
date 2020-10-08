fe0b4568524070284384612d7b5fc2904b1009b1
Seamus Ryan <d.seamus.ryan@outlook.com>
2020-10-08 00:01:44 -0400

    ronald

---
+++ chrome.js
@@ -0,0 +1,18 @@
+// let title = document.getElementsByTagName('title')
+// let value
+// title.innerText = value
+
+    
+//     var el = document.getElementsByTagName('html');
+//     var button = document.createElement("button");
+//     // button.innerHTML = "rename"
+//     el.appendChild(button);
+//     // document.createElement('button').appendChild( document.title );
+
+var button = document.getElementsByTagName("button");
+button.addEventListener('click', changeTitle)
+
+function changeTitle() {
+
+}
+
